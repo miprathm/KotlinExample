d78048e65fdcbf62cc804f846e16dad7d4c6c9ef
prathm92 <1prathm@gmail.com>
2018-02-27 13:43:03 +0530

    CR:Prathm: Git project setup

---
+++ app/src/main/java/prathm/com/kotlinexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package prathm.com.kotlinexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        nametextview?.setEnabled(true)
+
+
+
+    }
+}
